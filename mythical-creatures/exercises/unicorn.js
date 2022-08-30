c3a245c915465afa22d2bb31a28afb47b6e203e4
Justen Anderson <justen.anderson@gmail.com>
2022-08-29 18:50:00 -0600

    Update completed unicorn tests and the associated test documents

--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -1,8 +1,28 @@
 class Unicorn {
   constructor(name, color) {
     this.name = name;
-    this.color = color;
+    this.color = color || 'white';
+    }
+    isWhite() {
+      return this.color === 'white';
+    }
+    says(unicornSays) {
+      return '**;* ' + unicornSays + ' *;**'
+    }
   }
-}
+  console.log('Unicorn name and color: ', )
+
 
 module.exports = Unicorn;
+
+
+
+// 1 : Read the test
+// 2 : ask yourself if it's going to fail
+// 3: Run test
+// 4: make it pass
+
+// assert is our assertion library
+// equal is a method within it
+// 1st param to compare against
+// 2nd param is what's expected to create a true result
