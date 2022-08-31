1b5be4b6eb37a5969f0f0db3d706e55cf2f599e9
Justen Anderson <justen.anderson@gmail.com>
2022-08-31 13:57:06 -0600

    Update completed dragon exercises.

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,22 @@
+class Dragon {
+    constructor(name, rider) {
+    this.name = name;
+    this.rider = rider
+    this.hungry = true;
+    this.needsToEat = 0;
+    }
+    greet() {
+        return `Hi, ${this.rider}!`
+    }
+    eat() {
+        this.needsToEat++;
+        console.log('Hungry before: ', this.hungry)
+        this.needsToEat < 3 ? this.hungry = true : this.hungry =  false
+        console.log('Hungry after: ', this.hungry)
+        console.log('Needs to eat value: ', this.needsToEat)
+        }
+        
+    }
 
+
+module.exports =  Dragon;
