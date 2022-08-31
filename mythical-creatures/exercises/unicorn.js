3ea65219dc78abad298f91df23a0592292f80977
Justen Anderson <justen.anderson@gmail.com>
2022-08-31 13:56:28 -0600

    Remove comments from Unicorn exercise.

--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -17,12 +17,4 @@ module.exports = Unicorn;
 
 
 
-// 1 : Read the test
-// 2 : ask yourself if it's going to fail
-// 3: Run test
-// 4: make it pass
 
-// assert is our assertion library
-// equal is a method within it
-// 1st param to compare against
-// 2nd param is what's expected to create a true result
